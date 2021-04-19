fd9472b67c955f85a4879610d30c9747ac584a2c
datsfilipe <datsfilipe@gmail.com>
2021-04-19 15:52:32 -0300

    start nlw05 - part 1: server, typescript and express library

---
+++ README.md
@@ -0,0 +1,4 @@
+# NLW #05 - Trilha NODE
+
+Conteúdo adicionado até agora:
+- Aula 01;
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json(
+    {
+      message: "olá nlw 05"
+    }
+  );
+});
+
+app.post("/users", (request, response) => {
+  return response.json({
+    message: "Usuário salvo com sucesso"
+  });
+});
+
+app.listen(3333, () => {
+  console.log("The server is running on port 3333")
+}) ;
